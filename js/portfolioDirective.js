ace496ca9a7e0ad6c6d7c4e045e64734641d6d50
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-06-07 14:36:53 -0600

    made the twitter clone link

--- js/portfolioDirective.js
+++ js/portfolioDirective.js
@@ -8,12 +8,16 @@ angular.module('app').directive('portfolioDirective', function() {
       elem.find('h2').css('color', 'rgba(255, 255, 255, 1)')
       elem.css('cursor', 'pointer')
       elem.find('h2').css('transition', '1s')
-
     })
     elem.on('mouseleave', function(){
       elem.css('box-shadow', 'none')
       // elem.find('h2').css('font-size', '3.5rem')
       elem.find('h2').css('color', 'rgba(255, 255, 255, .0)')
+    })
+    elem.on('click', function(){
+      if(elem.find('h2').text() ==='Twitter Clone') {
+        window.open('http://taylormunnerlyn.github.io/twitter-clone', '_blank')
+      }
     })
 
   }

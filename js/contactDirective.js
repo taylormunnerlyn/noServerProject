240eedaa9400392c87eeecdd6206c87771a8c72f
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-06-07 12:00:14 -0600

    fifth no server commit

--- js/contactDirective.js
+++ js/contactDirective.js
@@ -9,12 +9,15 @@ angular.module('app').directive('contactDirective', function() {
       elem.css('cursor', 'pointer')
       elem.find('h2').css('transition', '.2s')
       elem.find('h3').css('color', 'rgba(255, 255, 255, 1)')
+      elem.find('a').css('color', 'rgba(255, 255, 255, 1)')
 
     })
     elem.on('mouseleave', function(){
       elem.css('box-shadow', 'none')
       // elem.find('h2').css('font-size', '6rem')
       elem.find('h3').css('color', 'rgba(255, 255, 255, .0)')
+      elem.find('a').css('color', 'rgba(255, 255, 255, .0)')
+
     })
 
   }
